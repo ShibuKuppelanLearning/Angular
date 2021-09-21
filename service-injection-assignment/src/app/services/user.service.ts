13eeeb69133f180d98323a544323609f24964b48
ShibuKuppelanLearning <shibu.kuppelan.learning@gmail.com>
2021-09-21 07:10:32 +0300

    service injection samples

--- service-injection-assignment/src/app/services/user.service.ts
+++ service-injection-assignment/src/app/services/user.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
 
+@Injectable({
+  providedIn: 'root'
+})
 export class UserService {
   activeUsers: string[] = ['Max', 'John'];
   inActiveUsers: string[] = ['James', 'Rusov'];
 
   constructor() { }
 
-  setToActive(id: number) {    
+  setToActive(id: number) {
     this.activeUsers.push(this.inActiveUsers[id]);
-    this.activeUsers.splice(id, 1);
-    
+    this.inActiveUsers.splice(id, 1);
+
   }
 
-  setToInActive(id: number) {    
+  setToInActive(id: number) {
     this.inActiveUsers.push(this.activeUsers[id]);
-    this.inActiveUsers.splice(id, 1);
+    this.activeUsers.splice(id, 1);
   }
 }

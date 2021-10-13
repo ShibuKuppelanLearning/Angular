b237ec1a7eeb4016ed1735e1338d4c38d596cf50
ShibuKuppelanLearning <shibu.kuppelan.learning@gmail.com>
2021-10-13 07:11:23 +0300

    template driven form changes

--- template-driven-forms/src/app/app.component.ts
+++ template-driven-forms/src/app/app.component.ts
@@ -8,11 +8,46 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('frm') form: NgForm;
+  defaultQuestion: string = 'teacher';
+  questionAnswer: string = '';
+  gender: string = '';
+  user: { username: string, mail: string, secretQuestion: string, answer: string, gender: string };
+
+  constructor() {
+    this.user = { username: '', mail: '', secretQuestion: '', answer: '', gender: '' };
+  }
   // onSubmit(form: NgForm) {
   //   console.log(form);
   // }
 
+  suggestName(): void {
+    const user = "super-user";
+    // this.form.form.setValue({
+    //   userData: {
+    //     username: user,
+    //     email: ""
+    //   },
+    //   gender: "",
+    //   secretQuestion: "",
+    //   questionAnswer: ""
+    // });
+    this.form.form.patchValue({
+      userData: {
+        username: user,
+      }
+    });
+  }
+
   onSubmit() {
     console.log(this.form);
+    this.user.username = this.form.value.userData.username;
+    this.user.mail = this.form.value.userData.email;
+    this.user.secretQuestion = this.form.value.secretQuestion;
+    this.user.answer = this.form.value.questionAnswer;
+    this.user.gender = this.form.value.gender;
+  }
+
+  onReset(){
+    this.form.reset();
   }
 }

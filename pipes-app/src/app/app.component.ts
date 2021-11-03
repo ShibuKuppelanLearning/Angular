9dc1455198ae791e9f4630f346e042f01f6c0aac
ShibuKuppelanLearning <shibu.kuppelan.learning@gmail.com>
2021-11-03 06:49:44 +0300

    pipe async changes

--- pipes-app/src/app/app.component.ts
+++ pipes-app/src/app/app.component.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  appStatus: Promise<string> = new Promise<string>((resolve, reject) => {
+    setInterval(() => {
+      resolve("stable");
+    }, 3000);
+  });
+
   servers: { name: string, instanceType: string, creationDate: Date, status: string }[] = [
     { name: 'Production', instanceType: 'medium', creationDate: new Date(1987, 5, 17), status: 'stable' },
     { name: 'User Database', instanceType: 'large', creationDate: new Date(2016, 1, 23), status: 'stable' },
@@ -13,9 +19,18 @@ export class AppComponent {
     { name: 'Testing Environmnet Server', instanceType: 'small', creationDate: new Date(2015, 9, 10), status: 'stable' }
   ];
 
-  filterByStatus: string='';
+  filterByStatus: string = '';
 
   getStatusClasses(server: { name: string, instanceType: string, creationDate: Date, status: string }) {
     return { 'list-group-item-success': server.status === 'stable', 'list-group-item-warning': server.status === 'offline', 'list-group-item-danger': server.status === 'critical' }
+  }
+
+  onAddServer() {
+    this.servers.push({
+      name: 'New Filter Server',
+      instanceType: 'medium',
+      creationDate: new Date(2021, 5, 17),
+      status: 'stable'
+    });
   }
 }
--- pipes-app/src/app/filter.pipe.ts
+++ pipes-app/src/app/filter.pipe.ts
@@ -1,16 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: { name: string, instanceType: string, creationDate: Date, status: string }[], filterString: string): { name: string, instanceType: string, creationDate: Date, status: string }[] {
+  transform(value: { name: string, instanceType: string, creationDate: Date, status: string }[], filterString: string, propName: string): { name: string, instanceType: string, creationDate: Date, status: string }[] {
     if (value.length === 0) {
       return value;
     }
     return value.filter(server => {
-      return server.status.startsWith(filterString);
+      return server[propName as keyof { name: string, instanceType: string, creationDate: Date, status: string }].toString().startsWith(filterString);
     });
   }
 }
